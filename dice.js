f862226b8564d1953e8c14b71b503f7ead1ab177
Myra Smith <myra.fowlkes.smith@gmail.com>
2021-03-02 13:22:56 -0500

    testing update

--- dice.js
+++ dice.js
@@ -1,9 +1,19 @@
+// My SMART Goal is:
+
+// Refactor my code so that by 12:00pm 3/2/21 I can store the value of the user input as a number and console log that value.
+
+// Stretch goals:
+
+// Create and console log a string of random dice that is the appropriate length
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const btn = document.querySelector('button')
     btn.addEventListener('click', () => {
         console.log('clicked!')
         const input = document.querySelector('input')
-        console.log(input)
+        console.log(input.value)
     })
+
 })
 
